2025c6dcc908dd2c1ecd5663d3d7717fe743a022
toanhoinon207 <nguyenductoan450@gmail.com>
2026-07-14 13:56:01 +0700

    Delete Session15 directory

--- Session15/schemas.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel, Field
-
-class EmployeeResponse(BaseModel):
-    full_name: str = Field(..., min_length = 1)
-    department: str = Field(..., min_length = 1)
-    position: str = Field(..., min_length = 1)
-    salary: float = Field(..., ge = 0)
